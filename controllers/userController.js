0a24184bd521cb6b35ed544423bb3c52a9b34c7c
Sumitha Naidu <sumitha.n@orosoft.com>
2026-02-27 12:35:13 +0530

    handled static routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,13 @@ const User = require("../models/User");
 
 // PROFILE
 exports.getProfile = async (req, res) => {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 // CREATE USER
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,12 @@ const authMiddleware = require("../middleware/authMiddleware");
 // Protect all user routes
 router.use(authMiddleware);
 
+router.get("/profile", userController.getProfile);
+
 router.post("/", userController.createUser); // POST /api/users
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
-router.get("/profile", userController.getProfile);
 
 module.exports = router;
